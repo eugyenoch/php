8099232d2b33cd30dac1d712853c7e7b2e1c199a
David U. Enoch <38564978+eugyenoch@users.noreply.github.com>
2020-03-19 11:48:11 +0100

    PHP functions
    The different ways to declare functions and their parameters in PHP - for educational purposes.
    Each PHP script can be in a different file if need be.

---
+++ educational/functions.php
@@ -0,0 +1,56 @@
+<?php
+//Declaring functions in PHP and passing parameters - for educational purposes
+?>
+
+<?php
+//Passing argument to a fnction by value
+function myFunction($A,$B){
+	$C=$A-$B;
+	echo"The difference between $A and $B is $C";
+	echo"<br>";
+}
+myFunction(50,30)
+?>
+
+<?php
+//Passing argument to a function by setting a default value
+function myFunction2($G=100,$H=90){
+	$I=$G-$H;
+	echo"The difference between $G and $H is $I";
+	echo"<br>";
+}
+myFunction2()
+?>
+
+<?php
+//Passing argument to a function by reference
+function myFunction3(&$J,&$K){
+	$L=$J-$K;
+	echo"The difference between $J and $K is $L";
+	echo"<br>";
+}
+?>
+
+<?php
+function MyFunction5($X){
+	return $X*$X;
+}
+echo "<br>";
+echo "The square of 5 is: ". myFunction(6);
+echo "<br>"
+
+?>
+
+
+<?php
+//Function without an argument
+function myFunction4(){
+	$D=50;
+	$E=30;
+	$F=$D-$E;
+	echo"The difference between $D and $E is $F";
+	echo"<br>";
+}
+myFunction4()
+
+?>
