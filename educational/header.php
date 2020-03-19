a3c66f4613678556788a808a0d04b213d7f3e659
David U. Enoch <38564978+eugyenoch@users.noreply.github.com>
2020-03-19 11:54:28 +0100

    PHP headers
    Headers in PHP are used to influence page behaviour. Examples are given herein on different PHP header scripts that achieve different tasks

---
+++ educational/header.php
@@ -0,0 +1,21 @@
+<?php
+//Redirects on pageload
+header("Location:https://loggerbros.com/");
+exit;
+
+//Creates a download on pageload
+//header("Content-Type:application/pdf");
+//header("Content-Disposition:attachement;filename='kotobee-services-whitepaper.pdf'");
+//readfile("kotobee-services-whitepaper.pdf");
+
+//Specify header expiration
+//header("Expires:Mon, 26 jun 2020, 10:50:55GMT");
+//header("Cache-Control:no-cache");
+//header("Pragma:no-cache");
+
+//Unset a header
+//header_remove("Expires");
+
+//List headers that would be sent in a HTTP transfer
+//headers_list();
+?>
